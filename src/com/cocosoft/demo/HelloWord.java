50c5a75214c7d7cb42c6dd74f485c15ea19bba47
wowtyphoon <wang_juntong@sina.com>
2016-09-11 15:52:43 +0800

    Initial commit

---
+++ src/com/cocosoft/demo/HelloWord.java
@@ -0,0 +1,11 @@
+package com.cocosoft.demo;
+
+/**
+ * Created by wangjt on 2016/9/11.
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
